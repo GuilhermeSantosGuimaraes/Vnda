727d592f1a538a55aca46f72590ba54548131acc
Guilherme Guimarães <guimaraes11santos1@gmail.com>
2021-06-20 00:36:06 -0300

    update
    atualização do banner e adicionando efeitos

--- vnda/src/js/swiper.js
+++ vnda/src/js/swiper.js
@@ -1,20 +1,13 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
